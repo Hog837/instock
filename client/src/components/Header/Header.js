917d5fb8829d2e64a38bc320b7ec76313e54c40e
Violetta Petrova <vwondercame@gmail.com>
2021-09-08 00:19:44 -0400

    add header mobile styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
+import Logo from "../../assets/Logo/InStock-Logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div>
-            
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-container__logo">
+          <Link to="/" className="header__logo-link">
+            <img className="header__logo-image" src={Logo} alt="Instock logo" />
+          </Link>
         </div>
-    )
+      </div>
+
+      <nav className="nav">
+        <div className="nav-container nav-container__warehouses">
+          <Link to="/warehouse" className="nav-container__link">Warehouses</Link>
+        </div>
+        <div className="nav-container nav-container__inventory">
+          <Link to="/inventory" className="nav-container__link">Inventory</Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;

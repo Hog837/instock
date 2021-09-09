5173d1c728d41c921920975dafced938b3d6099b
Jonah Hogate <Jonah.hogate@cox.net>
2021-09-09 14:55:14 -0500

    pulled j21jb-13

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Inventory from './pages/inventory/Inventory';
+import Inventory from './pages/Inventory/Inventory';
 import WareHouse from './pages/warehouse/WareHouse';
 import './App.scss'
 import Header from './components/Header/Header';

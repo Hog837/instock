7e498fa217b6959f5d4601017323c219616de38c
Jonah Hogate <Jonah.hogate@cox.net>
2021-09-09 14:57:33 -0500

    adding chenges to warehouse.js

--- client/src/pages/warehouse/WareHouse.js
+++ client/src/pages/warehouse/WareHouse.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import NewWarehouse from '../../components/NewWarehouse/NewWarehouse';
-//import WarehouseList from '../../components/WarehouseList/WarehouseList';
-//import './newWarehouse.scss'
-//import "./WareHouse.scss";
+import WarehouseList from '../../components/WarehouseList/WarehouseList'
+import "./WareHouse.scss";
 
 export default function WareHouse() {
     return (
-        <div>
-            <NewWarehouse/>
+        <div className="page">
+            <WarehouseList/>
         </div>
     )
 }

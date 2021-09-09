873c00b477b342c8011b6239292b2f51e4bfefcb
Jonah Hogate <Jonah.hogate@cox.net>
2021-09-09 17:12:49 -0500

    added routes in the warehouse.js routes

---
+++ server/routes/warehouse.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const warehouseListFilePath = "./data/warehouse.json";
+
+const warehouseList = () => {
+  const viewList = fs.readFileSync(warehouseListFilePath);
+  const parsedWarehouseList = JSON.parse(viewList);
+  return parsedWarehouseList;
+};
+router.get("/", (_req, res) => {
+  try {
+    const list = warehouseList();
+    return res.status(200).json(list);
+  } catch (err) {
+    return res.status(500).json({ error: "File cannot be read." });
+  }
+});
+router.get("/:id", (req, res) => {
+  const viewWarehouse = warehouseList();
+  const selectedWarehouse = viewWarehouse.find((warehouse) => {
+    return warehouse.id === req.params.id;
+  });
+  console.log(viewWarehouse);
+  res.status(200).json(selectedWarehouse);
+});

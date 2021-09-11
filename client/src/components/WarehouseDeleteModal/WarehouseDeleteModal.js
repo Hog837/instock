1c5c917d0be06082c65d5445ed2397e3ce60653b
Chanho Kim <chanho@Chanhos-MacBook-Air.local>
2021-09-10 23:47:45 -0400

    changed react hooks to class states, delete button is now functional

--- client/src/components/WarehouseDeleteModal/WarehouseDeleteModal.js
+++ client/src/components/WarehouseDeleteModal/WarehouseDeleteModal.js
@@ -1,29 +1,32 @@
 import React from "react";
 import closeIcon from "../../assets/Icons/close-24px.svg";
 import "./WarehouseDeleteModal.scss";
-function WarehouseDeleteModal({display, onClose}) {
-  if(!display){
-    return null
-  }
+import axios from "../../../../server/node_modules/axios";
+function WarehouseDeleteModal({ handleModal, selectedWarehouse, handleDelete}) {
+  console.log(selectedWarehouse.id)
+  const url = "http://localhost:8080";
+  
   return (
-    <div className="modal-background" onClick={onClose}>
+    <div className="modal-background" onClick={()=> {handleModal(false)}}>
       <div className="delete-modal" onClick={e => e.stopPropagation()}>
         <div className="delete-modal__content">
-          <button className="invisible-button delete-modal__close-icon" onClick={onClose}><img
+          <button type="button" className="invisible-button delete-modal__close-icon" onClick={()=>{handleModal(false)}}>
+            <img
             src={closeIcon}
             alt="close"
           /></button>
-          <h2 className="delete-modal__header">Delete King West warehouse?</h2>
+          <h2 className="delete-modal__header">{`Delete ${selectedWarehouse.name} warehouse?`}</h2>
           <p className="delete-modal__text">
-            Please confirm that you'd like to delete the King West from the list
-            of warehouses. You won't be able to undo this action.
+            {`Please confirm that you'd like to delete the ${selectedWarehouse.name} from the list
+            of warehouses. You won't be able to undo this action.`}
           </p>
         </div>
         <div className="delete-modal-buttons">
-          <button className="delete-modal-buttons__button delete-modal-buttons__cancel-button" onClick={onClose}>
+          <button type="button" className="delete-modal-buttons__button delete-modal-buttons__cancel-button" onClick={()=>{handleModal(false)}}>
             Cancel
           </button>
-          <button className="delete-modal-buttons__button delete-modal-buttons__delete-button">
+          <button type="button" className="delete-modal-buttons__button delete-modal-buttons__delete-button" onClick={()=>{handleDelete(selectedWarehouse.id)
+          handleModal(false)}}>
             Delete
           </button>
         </div>
--- client/src/components/WarehouseList/WarehouseList.js
+++ client/src/components/WarehouseList/WarehouseList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
 import chevronRightIcon from "../../assets/Icons/chevron_right-24px.svg";
@@ -9,115 +9,169 @@ import axios from "../../../../server/node_modules/axios";
 
 const url = "http://localhost:8080";
 
-function WarehouseList() {
-  const [display, setDisplay] = useState(false);
-  const [data, setData] = useState([]);
+class WarehouseList extends Component {
+  state = {
+    data: [],
+    selectedWarehouse: [],
+    display: false,
+  };
 
-  useEffect(() => {
+  componentDidMount(){
+    this.getData();
+  }
+
+  getData = () => {
     axios.get(`${url}/warehouse`).then((response) => {
-      setData(response.data);
-      console.log(response.data)
+      this.setState({
+        data: response.data,
+      });
     });
-  }, []);
+  };
 
-  return (
-    <div className="page">
-      <div className="warehouse-lists">
-        <form className="page-form">
-          <h1 className="page-form__title">Warehouses</h1>
-          <input
-            className="page-form__input"
-            type="text"
-            name="search"
-            placeholder="Search..."
-          ></input>
-          <button className="page-form__button" type="submit">
-            + Add New Warehouse
-          </button>
-        </form>
-        <nav className="page-nav-bar">
-          <p className="page-nav-bar__text">WAREHOUSE</p>
-          <img
-            className="page-nav-bar__sort-icon"
-            src={sortIcon}
-            alt="sort icon"
-          />
-          <p className="page-nav-bar__text">ADDRESS</p>
-          <img
-            className="page-nav-bar__sort-icon"
-            src={sortIcon}
-            alt="sort icon"
-          />
-          <p className="page-nav-bar__text">CONTACT NAME</p>
-          <img
-            className="page-nav-bar__sort-icon"
-            src={sortIcon}
-            alt="sort icon"
-          />
-          <p className="page-nav-bar__text">CONTACT INFORMATION</p>
-          <img
-            className="page-nav-bar__sort-icon"
-            src={sortIcon}
-            alt={sortIcon}
-          />
-          <p className="page-nav-bar__text">ACTIONS</p>
-        </nav>
-        <ul className="warehouse-list">
-          {data.map((warehouse) => {
-            return(
-            <li className="warehouse-item">
-              <div className="warehouse-item__texts">
-                <div className="warehouse-item__left">
-                  <div className="warehouse-item__left-1">
-                    <p className="warehouse-item__sub-title">WAREHOUSE</p>
-                    <p className="warehouse-item__warehouse-name">
-                      {warehouse.name}
-                      <img src={chevronRightIcon} alt="chevron right icon" />
-                    </p>
+  handleModal = (handler) => {
+    console.log(handler);
+    if (handler) {
+      this.setState({
+        display: true,
+      });
+    } else {
+      this.setState({
+        display: false,
+      });
+    }
+  };
+
+  handleID = (selectedWarehouse) => {
+    this.setState({
+      selectedWarehouse: selectedWarehouse,
+    });
+  };
+
+  handleDelete = (id) => {
+    axios.delete(`${url}/warehouse/${id}`)
+    .then(() => {
+      this.getData()
+    });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <div className="warehouse-lists">
+          <form className="page-form">
+            <h1 className="page-form__title">Warehouses</h1>
+            <input
+              className="page-form__input"
+              type="text"
+              name="search"
+              placeholder="Search..."
+            ></input>
+            <button className="page-form__button" type="submit">
+              + Add New Warehouse
+            </button>
+          </form>
+          <nav className="page-nav-bar">
+            <p className="page-nav-bar__text">WAREHOUSE</p>
+            <img
+              className="page-nav-bar__sort-icon"
+              src={sortIcon}
+              alt="sort icon"
+            />
+            <p className="page-nav-bar__text">ADDRESS</p>
+            <img
+              className="page-nav-bar__sort-icon"
+              src={sortIcon}
+              alt="sort icon"
+            />
+            <p className="page-nav-bar__text">CONTACT NAME</p>
+            <img
+              className="page-nav-bar__sort-icon"
+              src={sortIcon}
+              alt="sort icon"
+            />
+            <p className="page-nav-bar__text">CONTACT INFORMATION</p>
+            <img
+              className="page-nav-bar__sort-icon"
+              src={sortIcon}
+              alt={sortIcon}
+            />
+            <p className="page-nav-bar__text">ACTIONS</p>
+          </nav>
+          <ul className="warehouse-list">
+            {this.state.data.map((warehouse) => {
+              return (
+                <li className="warehouse-item">
+                  <div className="warehouse-item__texts">
+                    <div className="warehouse-item__left">
+                      <div className="warehouse-item__left-1">
+                        <p className="warehouse-item__sub-title">WAREHOUSE</p>
+                        <p className="warehouse-item__warehouse-name">
+                          {warehouse.name}
+                          <img
+                            src={chevronRightIcon}
+                            alt="chevron right icon"
+                          />
+                        </p>
+                      </div>
+                      <div className="warehouse-item__left-2">
+                        <p className="warehouse-item__sub-title">ADDRESS</p>
+                        <p className="warehouse-item__text">
+                          {`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="warehouse-item__right">
+                      <div className="warehouse-item__right-1">
+                        <p className="warehouse-item__sub-title">
+                          CONTACT NAME
+                        </p>
+                        <p className="warehouse-item__text">
+                          {warehouse.contact.name}
+                        </p>
+                      </div>
+                      <div className="warehouse-item__right-2">
+                        <p className="warehouse-item__sub-title">
+                          CONTACT INFORMATION
+                        </p>
+                        <p className="warehouse-item__text">
+                          {warehouse.contact.phone}
+                        </p>
+                        <p className="warehouse-item__text">
+                          {warehouse.contact.email}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="warehouse-item__left-2">
-                    <p className="warehouse-item__sub-title">ADDRESS</p>
-                    <p className="warehouse-item__text">
-                      {`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}
-                    </p>
+                  <div className="warehouse-item__icons">
+                    <button
+                      type="button"
+                      className="warehouse-item__icon invisible-button"
+                      onClick={() => {
+                        this.handleModal(true);
+                        this.handleID(warehouse);
+                      }}
+                    >
+                      <img src={deleteIcon} alt="delete icon" />
+                    </button>
+                    <button type="button" className="invisible-button">
+                      <img src={editIcon} alt="edit icon" />
+                    </button>
                   </div>
-                </div>
-                <div className="warehouse-item__right">
-                  <div className="warehouse-item__right-1">
-                    <p className="warehouse-item__sub-title">CONTACT NAME</p>
-                    <p className="warehouse-item__text">{warehouse.contact.name}</p>
-                  </div>
-                  <div className="warehouse-item__right-2">
-                    <p className="warehouse-item__sub-title">
-                      CONTACT INFORMATION
-                    </p>
-                    <p className="warehouse-item__text">
-                      {warehouse.contact.phone}</p>
-                    <p className="warehouse-item__text">{warehouse.contact.email}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="warehouse-item__icons">
-                <button
-                  className="warehouse-item__icon invisible-button"
-                  onClick={() => setDisplay(true)}
-                >
-                  <img src={deleteIcon} alt="delete icon" />
-                </button>
-                <button className="invisible-button">
-                  <img src={editIcon} alt="edit icon" />
-                </button>
-              </div>
-            </li>)
-          })}
-        </ul>
-        <WarehouseDeleteModal
-          display={display}
-          onClose={() => setDisplay(false)}
-        />
+                </li>
+              );
+            })}
+          </ul>
+          {this.state.display && (
+            <WarehouseDeleteModal
+              handleModal={this.handleModal}
+              selectedWarehouse={this.state.selectedWarehouse}
+              handleDelete={this.handleDelete}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default WarehouseList;

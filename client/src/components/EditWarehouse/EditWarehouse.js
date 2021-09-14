92a8a125f2b866c1d26638f0df91cb40b104b6c7
Jonah Hogate <Jonah.hogate@cox.net>
2021-09-13 19:55:22 -0500

    added linking to backend

--- client/src/components/EditWarehouse/EditWarehouse.js
+++ client/src/components/EditWarehouse/EditWarehouse.js
@@ -1,55 +1,179 @@
 import "./EditWarehouse.scss";
-import logo from '../../assets/Icons/arrow_back-24px.svg'
+import logo from "../../assets/Icons/arrow_back-24px.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { Component } from "react";
 
-function EditWarehouse() {
-  return(
-    <div className="page">
-    <section className="container">
-      <div className="container__header">
-        <Link to="/" className="container__logo-link">
-        <img className="container__arrow" src={logo} alt=""/>
-        </Link>
-        <h1 className="container__header-text">Edit Warehouse</h1>
+const url = "http://localhost:8080";
+class EditWarehouse extends Component {
+  state = {
+    name: "",
+    address: "",
+    city: "",
+    country: "",
+    user: "",
+    position: "",
+    phone: "",
+    email: "",
+  };
+  componentDidMount() {
+    axios
+      .get(`${url}/warehouse/${this.props.match.params.id}`)
+      .then((response) => {
+        this.setState({
+          name: response.data.name,
+          address: response.data.address,
+          city: response.data.city,
+          country: response.data.country,
+          user: response.data.contact.name,
+          position: response.data.contact.position,
+          phone: response.data.contact.phone,
+          email: response.data.contact.email,
+        });
+      });
+  }
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    axios({
+      method: "PUT",
+      url: `${url}/warehouse/${this.props.match.params.id}`,
+      data: {
+        name: e.target.name.value,
+        address: e.target.address.value,
+        city: e.target.city.value,
+        country: e.target.country.value,
+        contact: {
+          name: e.target.user.value,
+          position: e.target.position.value,
+          phone: e.target.phone.value,
+          email: e.target.email.value,
+        },
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(
+      () => {
+        this.props.history.push("/");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div className="page">
+        <section className="container">
+          <form
+            onSubmit={(e) => {
+              this.onSubmitHandler(e);
+            }}
+          >
+            <div className="container__header">
+              <Link to="/" className="container__logo-link">
+                <img className="container__arrow" src={logo} alt="" />
+              </Link>
+              <h1 className="container__header-text">Edit Warehouse</h1>
+            </div>
+            <hr className="container__hr-top" />
+            <div className="container__body-container">
+              <div className="container__warehouse">
+                <h3 className="container__subheading">Warehouse Details</h3>
+                <p className="container__warehouse-text">Warehouse Name</p>
+                <input
+                  name="name"
+                  className="container__warehouse-input"
+                  type="text"
+                  required
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+                <p className="container__warehouse-text">Street Address</p>
+                <input
+                  name="address"
+                  className="container__warehouse-input"
+                  type="text"
+                  required
+                  value={this.state.address}
+                  onChange={this.handleChange}
+                />
+                <p className="container__warehouse-text">City</p>
+                <input
+                  name="city"
+                  className="container__warehouse-input"
+                  type="text"
+                  required
+                  value={this.state.city}
+                  onChange={this.handleChange}
+                />
+                <p className="container__warehouse-text">Country</p>
+                <input
+                  name="country"
+                  className="container__warehouse-input"
+                  type="text"
+                  required
+                  value={this.state.country}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <hr className="container__hr-middle" />
+              <div className="container__contact">
+                <h3 className="container__subheading">Contact Details</h3>
+                <p className="container__contact-text">Contact Name</p>
+                <input
+                  name="user"
+                  className="container__contact-input"
+                  type="text"
+                  required
+                  value={this.state.user}
+                  onChange={this.handleChange}
+                />
+                <p className="container__contact-text">Position</p>
+                <input
+                  name="position"
+                  className="container__contact-input"
+                  type="text"
+                  required
+                  value={this.state.position}
+                  onChange={this.handleChange}
+                />
+                <p className="container__contact-text">Phone Number</p>
+                <input
+                  name="phone"
+                  className="container__contact-input"
+                  type="tel"
+                  pattern="+^\d{10}$"
+                  required
+                  value={this.state.phone}
+                  onChange={this.handleChange}
+                />
+                <p className="container__contact-text">Email</p>
+                <input
+                  name="email"
+                  className="container__contact-input"
+                  type="email"
+                  required
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="container__buttons">
+              <button className="container__button-cancel">Cancel</button>
+              <button type="submit" className="container__button-save">
+                Save
+              </button>
+            </div>
+          </form>
+        </section>
       </div>
-      <hr className="container__hr-top"/>
-      <div className="container__body-container">
-      <div className="container__warehouse">
-        <h3 className="container__subheading">Warehouse Details</h3>
-        <form>
-          <p className="container__warehouse-text">Warehouse Name</p>
-          <input className="container__warehouse-input" placeholder="King Weast" type="text" required></input>
-          <p className="container__warehouse-text">Street Address</p>
-          <input className="container__warehouse-input" placeholder="469 King Street West" type="text" required></input>
-          <p className="container__warehouse-text">City</p>
-          <input className="container__warehouse-input" placeholder="Torronto" type="text" required></input>
-          <p className="container__warehouse-text">Country</p>
-          <input  className="container__warehouse-input"placeholder="CAN" type="text" required></input>
-        </form>
-      </div>
-      <hr className="container__hr-middle"/>
-      <div className="container__contact">
-        <h3 className="container__subheading">Contact Details</h3>
-        <form>
-          <p className="container__contact-text">Contact Name</p>
-          <input className="container__contact-input" placeholder="Graeme Lyon" type="text" required></input>
-          <p className="container__contact-text">Position</p>
-          <input className="container__contact-input" placeholder="Warehouse Manager" type="text" required></input>
-          <p className="container__contact-text">Phone Number</p>
-          <input className="container__contact-input" placeholder="+1 (647) 504-0911" type="tel" pattern="^\d{10}$" required></input>
-          <p className="container__contact-text">Email</p>
-          <input className="container__contact-input" placeholder="glyon@instock.com" type="email" required></input>
-        </form>
-      </div>
-      </div>
-      <div className="container__buttons">
-        <form>
-          <button className="container__button-cancel">Cancel</button>
-          <button type="submit" className="container__button-save">Save</button>
-        </form>
-      </div>
-    </section>
-    </div>
-  );
-};
+    );
+  }
+}
 export default EditWarehouse;
